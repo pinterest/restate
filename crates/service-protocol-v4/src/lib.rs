bc95b48f16101b180ffe579dddd9e182d94bf0eb
Muhamad Awad <muhamad@restate.dev>
2026-07-08 10:54:31 +0300

    unresolved-future: Have an upper limit on maximum recursion
    Summary:
    This make sure sdks can't crash the runtime by sending a
    deeply nested unresolved-future, by setting a configurable
    maximum recursion limit

--- crates/service-protocol-v4/src/lib.rs
+++ crates/service-protocol-v4/src/lib.rs
@@ -76,6 +76,44 @@ pub mod proto {
             }
         }
 
+        impl super::Future {
+            // Gets the maximum depth of this future non-recursively
+            //
+            // To avoid possible stack overflow for deeply nested futures
+            // this function does some heap allocations. It's advised to
+            // calculate the depth once and keep it around if you need to
+            // do multiple calculations against it
+            pub fn depth(&self) -> usize {
+                let mut depth = 0;
+                let mut stack = vec![DepthStackFrame {
+                    inner: self,
+                    cursor: 0,
+                }];
+
+                while let Some(mut current) = stack.pop() {
+                    depth = depth.max(stack.len());
+                    if current.cursor >= current.inner.nested_futures.len() {
+                        continue;
+                    }
+
+                    let child = &current.inner.nested_futures[current.cursor];
+                    current.cursor += 1;
+                    stack.push(current);
+                    stack.push(DepthStackFrame {
+                        inner: child,
+                        cursor: 0,
+                    });
+                }
+
+                depth
+            }
+        }
+
+        struct DepthStackFrame<'a> {
+            inner: &'a super::Future,
+            cursor: usize,
+        }
+
         impl From<UnresolvedFuture> for super::Future {
             fn from(value: UnresolvedFuture) -> Self {
                 let (combinator, notifications, nested) = value.split();
@@ -137,6 +175,66 @@ pub mod proto {
 
                 builder.build().map_err(ConversionError::invalid_data)
             }
+        }
+    }
+
+    #[cfg(test)]
+    mod test {
+
+        #[test]
+        fn future_depth() {
+            // Fut([])
+            let fut = super::Future {
+                ..Default::default()
+            };
+
+            assert_eq!(fut.depth(), 0);
+
+            // Fut([Fut, Fut]) --> max depth is 1
+            let fut = super::Future {
+                nested_futures: vec![
+                    super::Future {
+                        ..Default::default()
+                    },
+                    super::Future {
+                        ..Default::default()
+                    },
+                ],
+                ..Default::default()
+            };
+
+            assert_eq!(fut.depth(), 1);
+
+            // Fut([Fut([Fut]), Fut]) --> max depth is 2
+            let fut = super::Future {
+                nested_futures: vec![
+                    super::Future {
+                        nested_futures: vec![super::Future {
+                            ..Default::default()
+                        }],
+                        ..Default::default()
+                    },
+                    super::Future {
+                        ..Default::default()
+                    },
+                ],
+                ..Default::default()
+            };
+
+            assert_eq!(fut.depth(), 2);
+
+            let mut fut = super::Future {
+                ..Default::default()
+            };
+
+            for _ in 0..10 {
+                fut = super::Future {
+                    nested_futures: vec![fut],
+                    ..Default::default()
+                };
+            }
+
+            assert_eq!(fut.depth(), 10);
         }
     }
 }
