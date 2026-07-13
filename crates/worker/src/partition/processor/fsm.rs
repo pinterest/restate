8eb924156371a5c9c2fdcd8c923239bcf3e6afb2
Ahmed Farghal <me@asoli.dev>
2026-07-13 10:00:46 +0100

    Phase 1 of redesigning the main partition loop
    Separates the partition-scoped commands from key-scoped commands. Currently, only minimum effort has been made into the key-scoped handler design. This will come in later phases. This PR focuses partition-scoped command handlers.
    
    The most notable changes are the new command handlers in `processor/commands/*`. The new design unblocks the ability to commit multiple
    transactions _within_ a single command handler when/as needed. Use of this will come in later PRs.

--- crates/worker/src/partition/processor/fsm.rs
+++ crates/worker/src/partition/processor/fsm.rs
@@ -8,9 +8,6 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
-// todo: remove when using this module
-#![allow(dead_code)]
-
 use std::sync::Arc;
 
 use tokio::sync::watch;
